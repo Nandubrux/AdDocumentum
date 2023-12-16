d5a464e62e914a31921610f22095178425cbceda
Nandubrux <nandubrux@gmail.com>
2023-12-16 08:12:55 -0300

    Update autobuy.py
    algumas modificações em funções simples no terminal para teste de funcionamento

--- Webdesigh/Multi/autobuy.py
+++ Webdesigh/Multi/autobuy.py
@@ -1,2 +1,14 @@
-print("meu primeiro emprego")
-print("conseguimos!")
+#print serve para imprimir no terminal
+#("") para adicionar oque deve ser impresso.
+#print("meu primeiro emprego")
+#print("conseguimos!")
+#print(2*51)
+#-------------------------------------------
+name = input("Digite seu nome: ")
+work = input("Digite seu cargo: ")
+age = input("Digite sua idade: ")
+#but input mean to get the empty space to white or get something inside.
+print("Resultado Final:")
+print(name), print(work), print(age)
+print("Parabens!!!")
+#------------------------------------------------
